0af568ff4a6a9088e199eb360c68eaace1c933c0
Elvenide <msffodomainer@gmail.com>
2025-10-18 18:34:52 -0400

    Added fade-in animation to Sections

--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import React from 'react';
+import { motion } from 'motion/react';
 
 type SectionProps = {
   children: React.ReactNode;
@@ -8,7 +11,9 @@ type SectionProps = {
 export default function Section({ children, className = "" }: SectionProps) {
   return (
     <section className={`relative z-20 py-6 lg:py-20 px-10 lg:px-24 w-full mx-auto ${className}`}>
-      {children}
+      <motion.div initial={{ opacity: 0 }} transition={{ duration: 1 }} whileInView={{ opacity: 1 }} className="w-full h-full">
+        {children}
+      </motion.div>
     </section>
   );
 };
