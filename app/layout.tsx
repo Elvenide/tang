bfade3582e135fcf154c83e1c29dd32e0d056993
Elvenide <msffodomainer@gmail.com>
2025-10-17 23:57:30 -0400

    Created AppService

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { AppServiceProvider } from "./lib/app-service";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
       >
-        {children}
+        <AppServiceProvider>{children}</AppServiceProvider>
       </body>
     </html>
   );
---
+++ app/lib/app-service.tsx
@@ -0,0 +1,23 @@
+
+import { createContext, useContext } from "react";
+
+interface IAppServiceContext {
+    currentPage: string;
+}
+
+const defaultValue = {
+    currentPage: "home"
+} satisfies IAppServiceContext;
+const AppServiceContext = createContext<IAppServiceContext>(defaultValue);
+
+export const AppServiceProvider = ({ children }: { children: React.ReactNode }) => {
+    return <AppServiceContext.Provider value={defaultValue}>{children}</AppServiceContext.Provider>;
+};
+
+export const useAppService = () => {
+    const context: IAppServiceContext = useContext(AppServiceContext);
+    if (!context) {
+        throw new Error("useAppService must be used within an AppServiceProvider");
+    }
+    return context;
+};
